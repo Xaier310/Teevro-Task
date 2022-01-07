5c341267ac02b24e2e701abba01f676099643d88
Xaier310 <83975334+Xaier310@users.noreply.github.com>
2022-01-07 13:03:24 +0530

    backend-completed

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+const transactionRoute = require("./routes/transaction");
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
+mongoose.connect(
+    process.env.MONGO_DB_LINK,
+    //  'mongodb://localhost:27017/Teevro', 
+    (err) => {
+      if (err) throw err;
+      console.log("DB Connected Successfully...");
+    }
+  );
+
+  app.use("/db/transaction-record", transactionRoute);
+
+app.listen(PORT || 3001,()=>{
+    console.log("Server is running on 3001");
+});
+
+app.get("/",(req,res)=>{
+  res.end("<h1>Server up and running</h1>");
+})
---
+++ routes/transaction.js
@@ -0,0 +1,44 @@
+const router = require("express").Router();
+const mongoose = require("mongoose");
+const { TransactionRecordSchema } = require("../models/TransactionsRecord");
+const TransactionRecord = require("../models/TransactionsRecord");
+
+// const TransactionRecord = new mongoose.model("TransactionRecord", TransactionRecordSchema);
+
+
+router.post("/",async (req,res)=>{
+    try{
+        let data = req.body;
+        if(data && data.transactionId && data.username){
+               let newTransaction = new TransactionRecord(req.body);
+               let savedTransaction = await newTransaction.save();
+               res.status(200).json(savedTransaction);
+        }
+        else{
+            res.status(500).json("data incomplete");
+        }
+    }
+    catch(err){
+        res.status(500).json("internal error");
+    }
+});
+
+router.get("/",async (req,res)=>{
+    try {
+    // console.log(req.query);
+    if(req.query){
+    var alltransactions = await TransactionRecord.find({username:req.query.username});
+    res.status(200).json(alltransactions);
+}
+else{
+    res.send(500).json("not authenticated");
+}
+}
+catch(err){
+    console.log(err);
+    res.send(500).json();
+}
+});
+
+
+module.exports = router;
